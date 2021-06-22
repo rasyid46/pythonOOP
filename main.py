d559c1a23fcfbe541b005fe691236cae4d0cef30
rasyid46 <sulaeman.rasyid@emveep.com>
2021-06-22 07:31:02 +0700

    Polymorpisem

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from geometri.bangun_ruang import BangunRuang
 from geometri.persegiPanjang import PersegiPanjang
 from geometri.segitiga import Segitiga
 
@@ -10,3 +11,18 @@ print(p1.hitung_luas())
 s1 = Segitiga(12,3)
 print(s1.info)
 print(s1.hitung_luas())
+
+
+print('\nMencoba membuat object dari kelas BangunRuang')
+b1 = BangunRuang()
+print(b1.info())
+print(b1.hitung_luas())
+
+# Polymorphism: kemampuan object untuk merespon berbeda, terhadap pemanggilan method yang sama
+daftar_bangun_ruang = []
+daftar_bangun_ruang.append(p1)
+daftar_bangun_ruang.append(s1)
+
+print('\nPolymorphism')
+for bangun_ruang in daftar_bangun_ruang:
+    print(bangun_ruang.info())
