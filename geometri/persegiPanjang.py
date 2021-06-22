e58ea74d6adfb2cfce31fb8e968ebe6cae466904
rasyid46 <sulaeman.rasyid@emveep.com>
2021-06-22 07:03:32 +0700

    Inheritence

--- geometri/persegiPanjang.py
+++ geometri/persegiPanjang.py
@@ -1,4 +1,7 @@
-class PersegiPanjang():
+from geometri.bangun_ruang import BangunRuang
+
+
+class PersegiPanjang(BangunRuang):
     def __init__(self,p,l):
         #fungsi yang dipanggil pertama kali
         self.p = p
--- geometri/segitiga.py
+++ geometri/segitiga.py
@@ -1,12 +1,15 @@
-class Segitiga():
-    def __init__(self,a,t):
-        #fungsi yang dipanggil pertama kali
+from geometri.bangun_ruang import BangunRuang
+
+
+class Segitiga(BangunRuang):
+    def __init__(self, a, t):
+        # fungsi yang dipanggil pertama kali
         self.a = a
-        self.t =t
+        self.t = t
 
     @property
     def info(self):
         return f'ini objek menghitung luas Segiga Alas {self.a} tinggi {self.t}'
 
     def hitung_luas(self):
-        return self.a*self.t /2
+        return self.a * self.t / 2
